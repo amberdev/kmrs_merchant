1a8f6d4dd15e85ff2a0d3647dad1a283832d5a63
Dipanshu <dipanshu.gupta@subcodevs.com>
2018-06-25 18:55:27 +0530

    kmrs merchant app first commit

---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={				
+    'ApiUrl':"https://dev3.foodsquire.com/merchantapp/api",	    
+	'DialogDefaultTitle' : "FoodSquire Merchant",
+	'pushNotificationSenderid': "491618252405",
+	'APIHasKey': "59af8bfae4fa5f1e78d1c1103d3e1b0f"
+};
